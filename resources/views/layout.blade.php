eee696635e76e247b07769c81366a8d2ce9ea271
ruchikushwah <ruchi741@bgpgmail.com>
2025-01-18 11:18:34 +0530

    header and layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,11 +1,17 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>hello world</h1>
+    @include('header')
+
+    @section('content')
+    @show
+
+    
 </body>
-</html> -->
+</html>
