bcccb6d0122074aa8a52c69ba607c4932a45d7e1
akankshaverm <priyaakanksha0507@gmail.com>
2025-01-20 14:59:35 +0530

    inter font will be added in tailwind config file

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,6 @@ Route::prefix("auth")->group(function () {
     Route::controller(AuthController::class)->group(function () {
         Route::match(["get", "post"], "/login", "login")->name("auth.login");
         Route::match(["get", "post"], "/register", "register")->name("auth.register");
-        Route::get("/logout", "logout")->name("auth.logout");
+        
     });
 });
