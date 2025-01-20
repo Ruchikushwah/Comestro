e45998b200f884bf34b9a807aa047dd6127abd48
puja-03 <pujakumari11th2003@gmail.com>
2025-01-20 13:30:17 +0530

    footer page add and portfolio

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/footer', function () {
+    return view('footer'); 
+});
+
+Route::get('/contact', function () {
+    return view('contact'); 
+});
