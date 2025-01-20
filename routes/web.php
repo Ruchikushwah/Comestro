911394cb2d9ff1b93b22c5d74a89cf2b457d7997
akankshaverm <priyaakanksha0507@gmail.com>
2025-01-20 08:55:41 +0530

    create login and register page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
+});
+Route::prefix("auth")->group(function () {
+    Route::controller(AuthController::class)->group(function () {
+        Route::match(["get", "post"], "/login", "login")->name("auth.login");
+        Route::match(["get", "post"], "/register", "register")->name("auth.register");
+        Route::get("/logout", "logout")->name("auth.logout");
+    });
 });
