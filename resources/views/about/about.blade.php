eb15324171e6bbdfdfd63a37eebde9adac78fdbb
puja-03 <pujakumari11th2003@gmail.com>
2025-01-21 12:42:10 +0530

    image add

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -1,1 +1,13 @@
-<h1>Hello world </h1>
+@extends('layout')
+@section('content')
+
+<div class="w-full ">
+    <div>
+        <img src="\02-image-free-img.jpg" alt="" class="w-full">
+    </div>
+    <div>
+        <h2></h2>
+    </div>
+</div>
+
+@endsection
