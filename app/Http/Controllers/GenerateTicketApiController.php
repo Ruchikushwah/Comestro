397cfc2e405a1d4713627e412b9f10f7cf1923f5
khobabe <khobabe007@gmail.com>
2025-01-22 21:00:05 +0530

    generate tickets and problem category model guarded empty array

--- app/Http/Controllers/GenerateTicketApiController.php
+++ app/Http/Controllers/GenerateTicketApiController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\GeneateTicket;
 use Illuminate\Http\Request;
 
 class GenerateTicketApiController extends Controller
 {
-    //
+    public function store(Request $request){
+        $ticket = new GeneateTicket();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::POST('/support',[ProblemCateogryApiController::class, 'store'])->name('problem.store');
+Route::post('/support',[ProblemCateogryApiController::class, 'store'])->name('problem.store');
 Route::get('/support',[ProblemCateogryApiController::class, 'index'])->name('support.index');
 Route::get('/support/{id}',[ProblemCateogryApiController::class, 'show'])->name('support.show');
