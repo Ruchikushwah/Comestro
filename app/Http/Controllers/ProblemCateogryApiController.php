f7be525f4619f91c87f538c03f503022e7de9679
khobabe <khobabe007@gmail.com>
2025-01-22 16:13:42 +0530

    api created for the ProblemCategory submission

--- app/Http/Controllers/ProblemCateogryApiController.php
+++ app/Http/Controllers/ProblemCateogryApiController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProblemCategory;
 use Illuminate\Http\Request;
 
 class ProblemCateogryApiController extends Controller
 {
-    //
+    // to store the problem categories:
+    public function store(Request $request){
+        $problem = new ProblemCategory();
+        $problem->name = $request->name;
+        $problem->description = $request->description;
+        $problem->save();
+
+        return response()->json(['data' => $problem, 'success' => true, 'msg' => 'Problem Category added successfully']);
+    }
+
+    
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProblemCateogryApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::POST('/support',[ProblemCateogryApiController::class, 'store'])->name('problem.store');
