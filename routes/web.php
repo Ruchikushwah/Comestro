a2a69df9aab3faf9d594b2ca19e41e0322eb0193
akankshaverm <priyaakanksha0507@gmail.com>
2025-01-22 23:35:48 +0530

    some changes in route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/footer', function () {
     return view('footer'); 
