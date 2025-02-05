1c463adcd3921ce4cf84221bea16ec8d1573ea3d
akankshaverm <priyaakanksha0507@gmail.com>
2025-02-05 22:56:57 +0530

    taillwind configure

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
     plugins: [
@@ -7,12 +9,10 @@ export default defineConfig({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
-
-        tailwindcss(),
     ],
-    build: {
-        outDir: "public/build", // Ensure this matches your desired path
-        manifest: true,
+    css: {
+        postcss: {
+            plugins: [tailwindcss(), autoprefixer()],
+        },
     },
-    base: "/build/",
 });
