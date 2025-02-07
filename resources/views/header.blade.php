179ea7ef11dc5eaa4f83a51f2449f20de1ebef27
akankshaverm <priyaakanksha0507@gmail.com>
2025-02-07 15:35:06 +0530

    some changes

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -40,9 +40,7 @@
                     <img src="/default.jpg" class="w-8 h-8 rounded-full" alt="User Avatar">
 
 
-                    <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                    </svg>
+                   
                 </button>
 
                 <!-- Dropdown -->
