614f804c36858ccaa3eba5a6e0783e80899a730d
ruchikushwah <ruchi741@bgpgmail.com>
2025-02-10 23:21:27 +0530

    some changes in header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+<nav class="bg-white dark:bg-gray-100 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
     <div class="max-w-full px-10 flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
             <span class="self-center text-xl font-semibold whitespace-nowrap">
@@ -7,7 +7,7 @@
         </a>
 
         <!-- Mobile Menu Toggle -->
-        <button id="menu-toggle" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+        <button id="menu-toggle" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 ">
             <span class="sr-only">Open main menu</span>
             <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
@@ -16,25 +16,25 @@
 
         <!-- Navigation Links -->
         <div class="hidden w-full md:flex md:w-auto" id="navbar-dropdown">
-            <ul class="flex flex-col md:flex-row font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li><a href="/" class="block py-2 px-3 rounded-sm md:p-0 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</a></li>
-                <li><a href="{{ route('about')}}" class="block py-2 px-3 rounded-sm md:p-0 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a></li>
-                <li><a href="{{ route('support.tickets.manage') }}" class="block py-2 px-3 rounded-sm md:p-0 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Support</a></li>
-                <li><a href="{{ route('contactus')}}" class="block py-2 px-3 rounded-sm md:p-0 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a></li>
+            <ul class="flex flex-col md:flex-row font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:mt-0 md:border-0 md:bg-white ">
+                <li><a href="/" class="block py-2 px-3 rounded-sm md:p-0 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 ">Home</a></li>
+                <li><a href="{{ route('about')}}" class="block py-2 px-3 rounded-sm md:p-0 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 ">About</a></li>
+                <li><a href="{{ route('support.tickets.manage') }}" class="block py-2 px-3 rounded-sm md:p-0 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 ">Support</a></li>
+                <li><a href="{{ route('contactus')}}" class="block py-2 px-3 rounded-sm md:p-0 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 ">Contact</a></li>
 
                 <!-- User Dropdown -->
                 <div class="hidden md:block relative">
                     @auth
                     <div class="relative">
                         <!-- User button -->
-                        <button class="flex items-center space-x-2 text-gray-900 dark:text-gray-100 focus:outline-none" id="user-menu-button">
+                        <button class="flex items-center space-x-2 text-gray-900 " id="user-menu-button">
                             <img src="/default.jpg" class="w-8 h-8 rounded-full" alt="User Avatar">
                         </button>
 
                         <!-- Dropdown -->
-                        <div id="user-dropdown" class="hidden absolute right-0 mt-2 w-56 bg-white border rounded-md shadow-lg dark:bg-gray-800">
-                            <span class="block px-4 py-2 text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700">{{ Auth::user()->name }}</span>
-                            <span class="block text-sm px-4 py-2 text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700">{{ Auth::user()->email }}</span>
+                        <div id="user-dropdown" class="hidden absolute right-0 mt-2 w-56 bg-white border rounded-md shadow-lg ">
+                            <span class="block px-4 py-2 text-gray-800  hover:bg-gray-200 ">{{ Auth::user()->name }}</span>
+                            <span class="block text-sm px-4 py-2 text-gray-800  hover:bg-gray-200 ">{{ Auth::user()->email }}</span>
                             <a href="{{ route('auth.logout') }}" class="block px-4 py-2 text-gray-800 hover:text-red-600">Logout</a>
                         </div>
                     </div>
