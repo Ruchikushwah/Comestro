aa6dec3c5f744bf1dd5132e101496ad3f1f6be1e
ruchikushwah <ruchi741@bgpgmail.com>
2025-02-11 17:45:20 +0530

    commit

--- routes/web.php
+++ routes/web.php
@@ -30,11 +30,6 @@ Route::get("/services", function () {
     return view('services');
 })->name("services");
 
-
-Route::get('/admin', function () {
-    return view('admin.dashboard');
-})->name('admin.dashboard');
-
 Route::prefix("admin")->group(function(){
     Route::get("/support/tickets", function () {
         return view("support.admin.ticketList", ['userName' => Auth::user()->name]);
